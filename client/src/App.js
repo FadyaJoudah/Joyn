d48ea74ac7ad8df6526483608cd8bb25c5b56a43
Fadya J <f.joudh95@gmail.com>
2022-01-13 21:22:39 -0800

    basic routing added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@
 
 import { Wrapper } from "@googlemaps/react-wrapper";
 import { Map } from "./components/GoogleMap/GoogleMap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import JoyninMap from "./components/Map/Map";
 const render = (status) => {
   return <h1>{status}</h1>;
@@ -13,9 +14,17 @@ const vancouver = {
 };
 function App() {
   return (
-    <div className="App">
-      <JoyninMap />
-    </div>
+    <Router>
+      <h1>nav</h1>
+      <Switch>
+        <Route path={"/"} exact>
+          <h1>Home</h1>
+        </Route>
+        <Route path={"/profile"}>
+          <h2>Profile</h2>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
