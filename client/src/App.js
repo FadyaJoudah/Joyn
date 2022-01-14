76fa8964a8238f44d4e60a6356f7ec5d677c90b0
Fadya J <f.joudh95@gmail.com>
2022-01-13 21:00:12 -0800

    add google maps

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+// import "./App.scss";
+// import { Map, WrappedMap } from "./components/Map/Map";
 
+import { Wrapper } from "@googlemaps/react-wrapper";
+import { Map } from "./components/GoogleMap/GoogleMap";
+import JoyninMap from "./components/Map/Map";
+const render = (status) => {
+  return <h1>{status}</h1>;
+};
+const vancouver = {
+  lat: 49.2600767,
+  lng: -123.1269348,
+};
 function App() {
-  return <div className="App"> this tis it</div>;
+  return (
+    <div className="App">
+      <JoyninMap />
+    </div>
+  );
 }
 
 export default App;
