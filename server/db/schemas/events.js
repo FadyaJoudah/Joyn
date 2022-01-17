cbe92777d926e85c9e124cbffa39aed86bbbc076
Fadya J <f.joudh95@gmail.com>
2022-01-16 23:50:42 -0800

    add comments on events routes

--- server/db/schemas/events.js
+++ server/db/schemas/events.js
@@ -1,3 +1,4 @@
+// query to add events to db
 function addEvent(
   userID,
   size,
@@ -12,4 +13,10 @@ function addEvent(
   const query = `INSERT INTO events(user_id,size,address,type,timestamp,description,pets_allowed,event_vaccination,location) VALUES ('${userID}','${size}','${address}','${type}','${timestamp}','${description}','${petsAllowed}','${vaccinationRequired}',Point(${location.lng},${location.lat}));`;
   return query;
 }
-module.exports = { addEvent };
+// query to get all the events to server
+function getAllEvents() {
+  const query = "SELECT * FROM events";
+  return query;
+}
+
+module.exports = { addEvent, getAllEvents };
--- server/routes/events.js
+++ server/routes/events.js
@@ -1,13 +1,24 @@
-const { Router } = require("express");
-const { addEvent } = require("../db/schemas/events");
+const { Router, query } = require("express");
+const { addEvent, getAllEvents } = require("../db/schemas/events");
 const jwt = require("jsonwebtoken");
 
 const events = Router();
 
+//endpoint to create new event
 events.post("/create", (req, res) => {
+  //aquiring the token from the request headers sent from the front end
   const token = req.headers.authorization;
+
+  //check if user is logged in
+  if (!token) {
+    res.status(401).send("you need to sign in first!");
+  }
+
+  //decoding the token to use user info inside it
   const decodedToken = jwt.verify(token, "peewee");
   const { userID } = decodedToken;
+
+  //getting db into scope after assigning it to req in index line :24
   const { db } = req;
   const {
     size,
@@ -19,9 +30,11 @@ events.post("/create", (req, res) => {
     vaccinationRequired,
     location,
   } = req.body;
+  // changing booleans data type
   let vaccine = vaccinationRequired === true ? 1 : 0;
   let petsFriendly = petsAllowed === true ? 1 : 0;
 
+  //query too add the event to db
   db.query(
     addEvent(
       userID,
@@ -38,6 +51,7 @@ events.post("/create", (req, res) => {
       if (err) {
         res.send("failed to create an event!");
       } else {
+        //select the id of the event
         db.query(`SELECT @@identity;`, (err, row2) => {
           const eventID = row2[0]["@@identity"];
           res.status(201).send({
@@ -56,6 +70,64 @@ events.post("/create", (req, res) => {
       }
     }
   );
+});
+
+events.get("/all", (req, res) => {
+  //deconstruct req to get db and headers
+  const { db, headers } = req;
+
+  // getting token from headers
+  const token = headers.authorization;
+  let decodedToken;
+
+  //checks if the user is logged in and takes the users info from token after decoding
+  if (token) {
+    decodedToken = jwt.verify(token, "peewee");
+  }
+  //query to get all the events in db
+  db.query(getAllEvents(), (err, rows, fields) => {
+    if (err) {
+      res.status(500).send("failed to load!");
+    } else {
+      //if the user is logged in they will have access to all events info
+      if (decodedToken) {
+        const allEvents = rows.map((row) => {
+          return {
+            id: row.id,
+            userID: row.user_id,
+            size: row.size,
+            address: row.address,
+            type: row.type,
+            timestamp: row.timestamp,
+            description: row.description,
+            petsFriendly: row.petsAllowed,
+            vaccine: row.event_vaccination,
+            location: { lng: row.location.x, lat: row.location.y },
+          };
+        });
+
+        res.send(allEvents);
+      } else {
+        //if the user is not logged in they will have some of the existing info of
+        const nonAuthEvents = rows.map((row) => {
+          return {
+            id: row.id,
+            userID: row.user_id,
+            // size: row.size,
+            // address: row.address,
+            // type: row.type,
+            // timestamp: row.timestamp,
+            description: row.description,
+            petsFriendly: row.petsAllowed,
+            // vaccine: row.event_vaccination,
+            location: { lng: row.location.x, lat: row.location.y },
+          };
+        });
+        s;
+        res.send(nonAuthEvents);
+      }
+    }
+  });
 });
 
 module.exports = events;
